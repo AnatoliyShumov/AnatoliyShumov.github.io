10413930154ca768da38c599fd1b370055193a1e
AnatoliyShumov <shumov@radioday.kpi.ua>
2020-01-27 12:57:39 +0200

    add files

---
+++ calendarApp/build/precache-manifest.b385bdee65015aebc53956a83c7467b9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce71f869ce235b914f2adc71401c82b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa86a60f8e3559517cd5",
+    "url": "./static/js/2.43a1b4a8.chunk.js"
+  },
+  {
+    "revision": "377a54e2b286864b83dc",
+    "url": "./static/js/main.270324a2.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
